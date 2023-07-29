3af2e6d11d10b2767107a37847fec399e102cdcf
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-29 12:01:01 -0400

    create a new class

---
+++ src/DemoGit/DemoGitTest.java
@@ -0,0 +1,12 @@
+package DemoGit;
+
+public class DemoGitTest {
+	
+	public static void main(String[] args) {
+		System.out.println("demo");
+	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
+}
