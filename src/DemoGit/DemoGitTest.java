335dbbb53ae8eaac43e80b58dfe92e711723dfd9
ermartin00 <140658480+ermartin00@users.noreply.github.com>
2023-07-29 12:02:12 -0400

    Update DemoGitTest.java

--- src/DemoGit/DemoGitTest.java
+++ src/DemoGit/DemoGitTest.java
@@ -9,4 +9,9 @@ public class DemoGitTest {
 	public static void add(int a, int b) {
 		System.out.println(a+b);
 	}
+
+	public static void add(int a, int b, int c) {
+		System.out.println(a+b+c);
+	}	
+	
 }
